84f968c78c45472126e121cb90f197128f174361
wessamoun <weso_1998@yahoo.com>
2022-02-19 05:55:53 +0200

    Install Sharp and configure endpoint.

--- src/routes/image.ts
+++ src/routes/image.ts
@@ -1,8 +1,29 @@
 import express from "express";
 const routes = express.Router();
-
+import sharp from "sharp";
+import path from "path";
+const root = path.resolve("");
 routes.get("/image", (req, res) => {
-  res.send("image");
+  const fun = async function () {
+    try {
+      await sharp(`${root}/assets/full/${req.query.filename as string}.jpg`)
+        .resize(Number(req.query.width), Number(req.query.height))
+        .toFile(
+          `${root}/assets/thumbs/${req.query.filename as string}-${Number(
+            req.query.width
+          )}-${Number(req.query.height)}.jpg`
+        );
+      res.sendFile(
+        `${root}/assets/thumbs/${req.query.filename as string}-${Number(
+          req.query.width
+        )}-${Number(req.query.height)}.jpg`
+      );
+    } catch (error) {
+      console.log(error);
+      res.send("Failed");
+    }
+  };
+  fun();
 });
 
 export default routes;
